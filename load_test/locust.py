972a21818f543bfb202f53567f7552d065d8a4ec
Edison Pizarro <edison.pizarro.godoy@gmail.com>
2023-10-20 20:34:12 -0300

    fix: only expose http port

--- load_test/locust.py
+++ load_test/locust.py
@@ -4,4 +4,4 @@ seed(1)
 class SumLoadTest(HttpUser):
     @task
     def sum_load_test(self):
-        self.client.post("http://producer:8000/sum", json={"number": randint(1000000, 50000000)})
+        self.client.post("http://nginx/producer/api/sum", json={"number": randint(1000000, 50000000)})
