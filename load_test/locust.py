227dfdb3707a27f8f48778b4aa8811bb29f4b934
Gerardo Crot Rojas <gicrot@uc.cl>
2024-05-10 08:56:06 -0400

    refactor: simplify celery workers

--- load_test/locust.py
+++
@@ -1,7 +0,0 @@
-from locust import HttpUser, task
-from random import randint,seed
-seed(1)
-class HelloWorldUser(HttpUser):
-    @task
-    def hello_world(self):
-        self.client.post("http://app:8000/job", json={"number": randint(1, 10000000)})
